f10083cad59e16a3d322e856df61d94b9204d021
Jorge Navarro <jnavarr56@gmail.com>
2020-06-16 08:33:21 -0400

    disable master key requirement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  # Not using this rails feature
+  config.require_master_key = false
 
   # In production, allow requests to the DNS.  
   config.hosts << ENV["PRODUCTION_HOST_DNS"]
